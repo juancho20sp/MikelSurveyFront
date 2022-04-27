61b77644eab80c44a9f38347841f96efef2f7476
Juan David Murillo <mikelsurvey2022@gmail.com>
2022-04-26 21:10:28 -0500

    Title added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Header = () => {
-  return <div className='text-white bg-gradient-to-br from-purple-600 to-blue-500 dark:focus:ring-blue-800 font-medium text-md text-center p-3'>
-        Título de la Encuesta
-      </div>;
+  return (
+    <div className='text-white bg-gradient-to-br from-purple-600 to-blue-500 dark:focus:ring-blue-800 font-medium text-md text-center p-3'>
+      Encuesta Pilares Institucionales
+    </div>
+  );
 };
 
 export default Header;
