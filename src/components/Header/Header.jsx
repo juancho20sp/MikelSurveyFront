7a3485246d6aa525671840b9cd94512f942466b3
Juan David Murillo <mikelsurvey2022@gmail.com>
2022-07-28 21:27:03 -0500

    Image styles added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from '../../logo.jpeg';
 
 const Header = () => {
+  const imageStyles = {
+    margin: '0 auto'
+  }
+
   return (
     <div className='text-white bg-gradient-to-br from-purple-600 to-blue-500 dark:focus:ring-blue-800 font-medium text-md text-center p-3'>
       Encuesta Pilares Institucionales
 
-      <img src={logo} alt="logo" />
+      <img style={imageStyles} src={logo} alt="logo" />
     </div>
   );
 };
